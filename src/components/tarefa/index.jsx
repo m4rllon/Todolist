c81ddb5062505c34f64d955898d2a85d00d4a6a3
m4rllon <marllonpc33@gmail.com>
2024-08-20 13:02:22 -0300

    test: Adicionar componente Tarefa

---
+++ src/components/tarefa/index.jsx
@@ -0,0 +1,15 @@
+export default function Tarefa() {
+  return (
+    <div className="w-[50rem]">
+      <span className="flex justify-between">
+        <div className="flex gap-3">
+          <input type="checkbox" name="" id="" />
+          <p>namenamename</p>
+        </div>
+        <div className="flex justify-end">icons</div>
+      </span>
+      <p>data</p>
+      <hr className="border-gray-500 border-2 mx-auto" />
+    </div>
+  );
+}
